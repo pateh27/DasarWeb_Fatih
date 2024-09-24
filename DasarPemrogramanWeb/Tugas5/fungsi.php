2d34e1b0ba43489f27f7b118fa5d190455433e26
pateh27 <fathzfl@gmail.com>
2024-09-24 09:08:16 +0700

    save as jquery

--- DasarPemrogramanWeb/Tugas5/fungsi.php
+++ DasarPemrogramanWeb/Tugas5/fungsi.php
@@ -1,21 +1,18 @@
 <?php 
-    function perkenalan($nama, $salam= "Assalamualaikum") {
-        echo  $salam. ", ";
-        echo "Perkenalkan, nama saya ".$nama. "<br>";
-        echo "Senang berkenalan dengan anda<br>";
-    }
-
-    perkenalan("Hamdana","Hallo");
-    echo"<hr>";
-
-    $saya = "Elok";
-    $ucapanSalam = "selamat pagi ";
-
-    perkenalan($saya,);
-
+    
     function hitungUmur($thn_lahir, $thn_sekarang) {
         $umur = $thn_sekarang - $thn_lahir;
         return $umur;
     }
-    echo "Umur saya adalah ". hitungUmur(2004, 2024). " tahun";
+    function perkenalan($nama, $salam= "Assalamualaikum") {
+        echo  $salam. ", ";
+        echo "Perkenalkan, nama saya ".$nama. "<br>";
+        
+        echo "Saya berusia " . hitungUmur(2004, 2024). " tahun <br>";
+        echo "Senang berkenalan dengan anda <br>";
+    }
+
+    
+    perkenalan("Elok");
+    echo "<hr>";
 ?>
