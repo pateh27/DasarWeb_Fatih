2458f4fc05bea10926e032db0e809f0487e1e78f
pateh27 <fathzfl@gmail.com>
2024-10-09 19:49:38 +0700

    lupda commit untuk beberapa file sebelum html injection

---
+++ DasarPemrogramanWeb/Tugas7/empty.php
@@ -0,0 +1,14 @@
+<?php 
+    $myArray = array();
+    if (empty($myArray)) {
+        echo "Array tidak terdefiinisikan atau kosong. <br>";
+    } else {
+        echo "Array terdefiinisikan dan tidak kosong. <br>";
+    }
+
+    if (empty($nonExistentVar)){
+        echo "Varibel tidak terdifinisikan atau kosong.<br>";
+    }else {
+        echo "Variable terdefiinisikan dan tidak kosong.<br>";
+    }
+?>
---
+++ DasarPemrogramanWeb/Tugas7/html_aman.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Form Input PHP</title>
+</head>
+<body>
+    <h2>Form Input PHP</h2>
+    <form method="post" action="proses_form.php">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required><br><br>
+
+        <input type="submit" name="submit" value="Submit">
+
+    </form>
+
+    <?php
+    // soal nomer 4.1
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+           $input = $_POST['input'];
+           $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+           echo "Nama: " . $input . "<br>";
+        }
+    ?>
+    <?php 
+        // soal nomer 4.2
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            $email = $_POST['email'];
+
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo "Email: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "<br>.";
+            }else{
+                echo "Email tidak valid.<br>";
+            }
+        }
+    ?>
+
+</body>
+</html>
