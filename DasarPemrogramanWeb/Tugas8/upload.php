49eccc2fc48727eb6699d8a1e9aca6d9c78b7800
pateh27 <fathzfl@gmail.com>
2024-10-16 19:33:19 +0700

    praktikum 1 selesai dan juga penambahan kode kode

---
+++ DasarPemrogramanWeb/Tugas8/upload.php
@@ -0,0 +1,24 @@
+<?php 
+    if (isset($_POST["submit"])) {
+        $targetdir = "uploads/";
+        $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+        $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxsize = 5*1024*1024;
+
+        if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+            
+        
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah";
+        // pertanyaan 6
+            //echo "<img src='" . $targetfile . "' widht='200' alt='thumbnail'>";
+        } else {
+            echo "Gagal mengunggah file";
+        }
+    } else{
+        echo "File tidak valid atau melebihi ukuran maksismum yang diizinkan";
+    }
+    }
+?>
